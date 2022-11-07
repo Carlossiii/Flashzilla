8e8dd9b9cbd5de4793a53fb979675ce71499569f
Carlossiii <cv5673@gmail.com>
2022-11-07 19:18:43 -0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Carlos Vinicius on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
